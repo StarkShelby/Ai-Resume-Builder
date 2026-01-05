e1141276343d717e3b5b68615fdd2d05a48f4de3
StarkShelby <your-email@example.com>
2026-01-05 20:25:00 +0530

    disable eslint

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001',
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
