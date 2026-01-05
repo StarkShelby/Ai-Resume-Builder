a9ab09350ef88b02c7e0e495a4fb698bef613e25
StarkShelby <your-email@example.com>
2026-01-05 20:47:40 +0530

    bug fixes

--- frontend/app/resume/editor/page.tsx
+++ frontend/app/resume/editor/page.tsx
@@ -1,7 +1,6 @@
+"use client"
+
 export const dynamic = "force-dynamic";
-
-
-"use client";
 
 import { useEffect, useState, useRef } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
